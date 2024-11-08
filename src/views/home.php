a1f1ca19b9d0e13f5542696cee15b172b5ef8d81
Lucy <lucy.madec@laplateforme.io>
2024-11-08 16:25:02 +0100

    Add favicon icon next to Cinetech text in header

--- src/views/home.php
+++ src/views/home.php
@@ -11,11 +11,14 @@
 
 <body>
     <header class="site-header">
-        <h1 class="neon-text">Cinetech</h1>
+        <h1 class="neon-text">
+            <img src="/cinetech/public/images/favicon.png" alt="Logo Cinetech" class="logo-icon">
+            Cinetech
+        </h1>
         <nav class="navbar">
-            <a href="?page=home">Accueil</a>
-            <a href="?page=movies">Films</a>
-            <a href="?page=series">Séries</a>
+            <a href="?page=home" class="neon-text">Accueil</a>
+            <a href="?page=movies" class="neon-text">Films</a>
+            <a href="?page=series" class="neon-text">Séries</a>
             <form action="?page=search" class="search-bar" method="get">
                 <input type="text" name="query" placeholder="Rechercher...">
                 <button type="submit">🔍</button>
