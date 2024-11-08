06902aa89b6c2966df1fe9bc31c987bc2a4c7ff4
Lucy <lucy.madec@laplateforme.io>
2024-11-08 13:16:24 +0100

    Enhance homepage with popular movies and series posters and grid layout

--- src/views/home.php
+++ src/views/home.php
@@ -14,28 +14,34 @@
 
     <div class="content-section">
         <h2>Films populaires</h2>
-        <?php if (!empty($popularMovies['results'])): ?>
-            <?php foreach ($popularMovies['results'] as $movie): ?>
-                <div class="item">
-                    <p><strong><?php echo htmlspecialchars($movie['title']); ?></strong></p>
-                </div>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <p>Aucun film populaire trouvé.</p>
-        <?php endif; ?>
+        <div class="items-grid">
+            <?php if (!empty($popularMovies['results'])): ?>
+                <?php foreach ($popularMovies['results'] as $movie): ?>
+                    <div class="item">
+                        <img src="https://image.tmdb.org/t/p/w200<?php echo htmlspecialchars($movie['poster_path']); ?>" alt="<?php echo htmlspecialchars($movie['title']); ?>">
+                        <p><strong><?php echo htmlspecialchars($movie['title']); ?></strong></p>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>Aucun film populaire trouvé.</p>
+            <?php endif; ?>
+        </div>
     </div>
 
     <div class="content-section">
         <h2>Séries populaires</h2>
-        <?php if (!empty($popularSeries['results'])): ?>
-            <?php foreach ($popularSeries['results'] as $series): ?>
-                <div class="item">
-                    <p><strong><?php echo htmlspecialchars($series['name']); ?></strong></p>
-                </div>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <p>Aucune série populaire trouvée.</p>
-        <?php endif; ?>
+        <div class="items-grid">
+            <?php if (!empty($popularSeries['results'])): ?>
+                <?php foreach ($popularSeries['results'] as $series): ?>
+                    <div class="item">
+                        <img src="https://image.tmdb.org/t/p/w200<?php echo htmlspecialchars($series['poster_path']); ?>" alt="<?php echo htmlspecialchars($series['name']); ?>">
+                        <p><strong><?php echo htmlspecialchars($series['name']); ?></strong></p>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>Aucune série populaire trouvée.</p>
+            <?php endif; ?>
+        </div>
     </div>
 </body>
 
