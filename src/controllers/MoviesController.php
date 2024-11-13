b7fa545ab20c56939b88d1fd0c401b771333dfe9
Lucy <lucy.madec@laplateforme.io>
2024-11-13 10:55:46 +0100

    Create movies page with popular movies display and include partial templates

--- src/controllers/MoviesController.php
+++ src/controllers/MoviesController.php
@@ -2,10 +2,23 @@
 
 namespace Controllers;
 
+use Models\ApiModel;
+
 class MoviesController
 {
+    private $apiModel;
+
+    public function __construct()
+    {
+        $this->apiModel = new ApiModel();
+    }
+    
     public function index()
     {
+        // Retrieving popular films from the API
+        $popularMovies = $this->apiModel->getPopularMovies();
+
+        // Passing data to the view
         require_once __DIR__ . '/../views/movies.php';
     }
 }
