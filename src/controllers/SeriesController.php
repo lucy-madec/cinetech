93e9aab779201a8f4737f9861fc4fedf51f5a32a
Lucy <lucy.madec@laplateforme.io>
2024-11-13 11:14:07 +0100

    Create series page with popular series display and include partial templates

--- src/controllers/SeriesController.php
+++ src/controllers/SeriesController.php
@@ -2,10 +2,23 @@
 
 namespace Controllers;
 
+use Models\ApiModel;
+
 class SeriesController
 {
+    private $apiModel;
+    
+    public function __construct()
+    {
+        $this->apiModel = new ApiModel();
+    }
+
     public function index()
     {
+        // Retrieving popular series from the API
+        $popularSeries = $this->apiModel->getPopularSeries();
+
+        // Passing data to the view
         require_once __DIR__ . '/../views/series.php';
     }
 }
