9b1edc20a1fbe17c56f8e5e7e7621f6d01471a7e
Lucy <lucy.madec@laplateforme.io>
2024-11-21 13:43:03 +0100

    Changed the locations for the login and logout to home page

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController
             $user = $this->userModel->login($email, $password);
             if ($user) {
                 $_SESSION['user_id'] = $user['id'];
-                header('Location: ?page=favoris');
+                header('Location: ?page=home');
                 exit;
             }
 
@@ -50,7 +50,7 @@ class AuthController
     public function logout()
     {
         session_destroy();
-        header('Location: ?page=login');
+        header('Location: ?page=home');
         exit;
     }
 }
